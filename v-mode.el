d9777707e28383ba32a75aabadee62989f590f0a
Damon Kwok <damon-kwok@outlook.com>
2020-08-08 12:21:54 +0800

    SPDX License Identifiers in Source

--- v-mode.el
+++ v-mode.el
@@ -1,15 +1,26 @@
 ;;; v-mode.el --- A major mode for the V programming language  -*- lexical-binding: t; -*-
-;;
+
+;; Copyright (c) 2020 Damon Kwok
+
 ;; Authors: Damon Kwok <damon-kwok@outlook.com>
 ;; Version: 0.0.1
 ;; URL: https://github.com/damon-kwok/v-mode
 ;; Keywords: languages programming
 ;; Package-Requires: ((emacs "25.1") (dash "2.17.0") (hydra "0.15.0"))
-;;
-;; This file is not part of GNU Emacs.
-;;
-;; Copyright (c) 2020 Damon Kwok
-;;
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;; Description:
