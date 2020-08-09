ff39fb8c3a0b857f694837b2c9f1e8fcce62cba9
Damon Kwok <damon-kwok@outlook.com>
2020-08-09 09:23:51 +0800

    Fix comment end

--- v-mode.el
+++ v-mode.el
@@ -450,7 +450,7 @@ Optional argument BUILD ."
   (setq-local require-final-newline mode-require-final-newline)
   (setq-local parse-sexp-ignore-comments t)
   (setq-local comment-start "/*")
-  (setq-local comment-start "*/")
+  (setq-local comment-end "*/")
   (setq-local comment-start-skip "\\(//+\\|/\\*+\\)\\s *")
   (setq-local electric-indent-chars (append "{}():;," electric-indent-chars))
   (setq-local indent-line-function #'js-indent-line)
